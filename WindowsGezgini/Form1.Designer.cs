c6deb5fc9348bb605b6824e8fc1817bb5b0cdc27
fadim <fadim.baylak@gmail.com>
2014-01-28 21:46:27 +0200

    init

---
+++ WindowsGezgini/Form1.Designer.cs
@@ -0,0 +1,323 @@
+﻿namespace WindowsGezgini
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.txtADRES = new System.Windows.Forms.ToolStripTextBox();
+            this.toolStripLabel2 = new System.Windows.Forms.ToolStripLabel();
+            this.txtARA = new System.Windows.Forms.ToolStripTextBox();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.lblDurum = new System.Windows.Forms.ToolStripStatusLabel();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.görünümToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.detayToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.küçükResimToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.büyükResimToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.kopyalaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.yapıştırToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.silToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLabel1,
+            this.txtADRES,
+            this.toolStripLabel2,
+            this.txtARA,
+            this.toolStripButton1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(542, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(40, 22);
+            this.toolStripLabel1.Text = "Adres:";
+            // 
+            // txtADRES
+            // 
+            this.txtADRES.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.txtADRES.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.FileSystem;
+            this.txtADRES.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.txtADRES.Name = "txtADRES";
+            this.txtADRES.Size = new System.Drawing.Size(200, 25);
+            this.txtADRES.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtADRES_KeyDown);
+            // 
+            // toolStripLabel2
+            // 
+            this.toolStripLabel2.Name = "toolStripLabel2";
+            this.toolStripLabel2.Size = new System.Drawing.Size(45, 22);
+            this.toolStripLabel2.Text = "Arama:";
+            // 
+            // txtARA
+            // 
+            this.txtARA.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.txtARA.Name = "txtARA";
+            this.txtARA.Size = new System.Drawing.Size(100, 25);
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(29, 22);
+            this.toolStripButton1.Text = "Ara";
+            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lblDurum});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 391);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(542, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // lblDurum
+            // 
+            this.lblDurum.Name = "lblDurum";
+            this.lblDurum.Size = new System.Drawing.Size(69, 17);
+            this.lblDurum.Text = "Dosya Ara...";
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 25);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.treeView1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.listView1);
+            this.splitContainer1.Size = new System.Drawing.Size(542, 366);
+            this.splitContainer1.SplitterDistance = 180;
+            this.splitContainer1.TabIndex = 2;
+            // 
+            // treeView1
+            // 
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView1.Location = new System.Drawing.Point(0, 0);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.PathSeparator = "";
+            this.treeView1.Size = new System.Drawing.Size(180, 366);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.listView1.ContextMenuStrip = this.contextMenuStrip1;
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.GridLines = true;
+            this.listView1.Location = new System.Drawing.Point(0, 0);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(358, 366);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.DoubleClick += new System.EventHandler(this.listView1_DoubleClick);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Dosya Adı";
+            this.columnHeader1.Width = 200;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Boyutu";
+            this.columnHeader2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.columnHeader2.Width = 75;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Klasör";
+            this.columnHeader3.Width = 200;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.görünümToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.kopyalaToolStripMenuItem,
+            this.yapıştırToolStripMenuItem,
+            this.silToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(126, 98);
+            // 
+            // görünümToolStripMenuItem
+            // 
+            this.görünümToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.listeToolStripMenuItem,
+            this.detayToolStripMenuItem,
+            this.küçükResimToolStripMenuItem,
+            this.büyükResimToolStripMenuItem});
+            this.görünümToolStripMenuItem.Name = "görünümToolStripMenuItem";
+            this.görünümToolStripMenuItem.Size = new System.Drawing.Size(125, 22);
+            this.görünümToolStripMenuItem.Text = "Görünüm";
+            // 
+            // listeToolStripMenuItem
+            // 
+            this.listeToolStripMenuItem.Name = "listeToolStripMenuItem";
+            this.listeToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.listeToolStripMenuItem.Text = "Liste";
+            this.listeToolStripMenuItem.Click += new System.EventHandler(this.listeToolStripMenuItem_Click);
+            // 
+            // detayToolStripMenuItem
+            // 
+            this.detayToolStripMenuItem.Name = "detayToolStripMenuItem";
+            this.detayToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.detayToolStripMenuItem.Text = "Detay";
+            this.detayToolStripMenuItem.Click += new System.EventHandler(this.detayToolStripMenuItem_Click);
+            // 
+            // küçükResimToolStripMenuItem
+            // 
+            this.küçükResimToolStripMenuItem.Name = "küçükResimToolStripMenuItem";
+            this.küçükResimToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.küçükResimToolStripMenuItem.Text = "Küçük Resim";
+            this.küçükResimToolStripMenuItem.Click += new System.EventHandler(this.küçükResimToolStripMenuItem_Click);
+            // 
+            // büyükResimToolStripMenuItem
+            // 
+            this.büyükResimToolStripMenuItem.Name = "büyükResimToolStripMenuItem";
+            this.büyükResimToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.büyükResimToolStripMenuItem.Text = "Büyük Resim";
+            this.büyükResimToolStripMenuItem.Click += new System.EventHandler(this.büyükResimToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(122, 6);
+            // 
+            // kopyalaToolStripMenuItem
+            // 
+            this.kopyalaToolStripMenuItem.Name = "kopyalaToolStripMenuItem";
+            this.kopyalaToolStripMenuItem.Size = new System.Drawing.Size(125, 22);
+            this.kopyalaToolStripMenuItem.Text = "Kopyala";
+            this.kopyalaToolStripMenuItem.Click += new System.EventHandler(this.kopyalaToolStripMenuItem_Click);
+            // 
+            // yapıştırToolStripMenuItem
+            // 
+            this.yapıştırToolStripMenuItem.Enabled = false;
+            this.yapıştırToolStripMenuItem.Name = "yapıştırToolStripMenuItem";
+            this.yapıştırToolStripMenuItem.Size = new System.Drawing.Size(125, 22);
+            this.yapıştırToolStripMenuItem.Text = "Yapıştır";
+            this.yapıştırToolStripMenuItem.Click += new System.EventHandler(this.yapıştırToolStripMenuItem_Click);
+            // 
+            // silToolStripMenuItem
+            // 
+            this.silToolStripMenuItem.Name = "silToolStripMenuItem";
+            this.silToolStripMenuItem.Size = new System.Drawing.Size(125, 22);
+            this.silToolStripMenuItem.Text = "Sil";
+            this.silToolStripMenuItem.Click += new System.EventHandler(this.silToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(542, 413);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Form1";
+            this.Text = "Windows Gezgini";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.ToolStripTextBox txtADRES;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel2;
+        private System.Windows.Forms.ToolStripTextBox txtARA;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel lblDurum;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem görünümToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem listeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem detayToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem küçükResimToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem büyükResimToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem kopyalaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem yapıştırToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem silToolStripMenuItem;
+    }
+}
+
---
+++ WindowsGezgini/Form1.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Collections;
+//Directory:Klasör
+//DriveInfo:Sürücü Bilgisi
+//FileInfo:Dosya Bilgisi
+//Path:Yol
+//File :Dosya
+
+namespace WindowsGezgini
+{
+    public partial class Form1 : Form
+    {
+        ArrayList liste = new ArrayList();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DriveInfo[] surucu=DriveInfo.GetDrives();
+            for (int i = 0; i < surucu.Length; i++)
+            {
+                TreeNode tn = new TreeNode();
+                tn.Text = surucu[i].Name;
+                treeView1.Nodes.Add(tn);
+                if (surucu[i].IsReady)
+                {
+                    KlasorListele(tn);    
+                }
+                
+            }
+        }
+
+        void KlasorListele(TreeNode tn)
+        {
+            string[] klasorler = Directory.GetDirectories(tn.Text);
+            for (int i = 0; i < klasorler.Length; i++)
+            {
+                TreeNode tn1 = new TreeNode();
+                tn1.Text = klasorler[i].Substring(3);
+                tn.Nodes.Add(tn1);
+            }
+        }
+
+        void DosyaListele(string yol, string filtre)
+        {
+            //
+            try
+            {
+                string[] dosyalar = Directory.GetFiles(yol, filtre);
+
+                for (int i = 0; i < dosyalar.Length; i++)
+                {
+                    ListViewItem li = new ListViewItem();
+                    li.Text = Path.GetFileName(dosyalar[i]);
+                    listView1.Items.Add(li);
+
+                    FileInfo fi = new FileInfo(dosyalar[i]);
+
+                    //li.SubItems.Add(fi.Length.ToString());
+
+                    string uzunluk = "";
+                    if (fi.Length>1024*1024*1024)
+                    {
+                        uzunluk = Convert.ToString(fi.Length / (1024 * 1024 * 1024)) +"GB";
+                    }
+                    else if (fi.Length > 1024 * 1024 )
+                    {
+                        uzunluk = Convert.ToString(fi.Length / (1024 * 1024)) + "MB";
+                    }
+                    else if (fi.Length > 1024 )
+                    {
+                        uzunluk = Convert.ToString(fi.Length / 1024) + "KB";
+                    }
+                    else
+                    {
+                        uzunluk = Convert.ToString(fi.Length) + "Bytes";
+                    }
+                    li.SubItems.Add(uzunluk);
+                    li.SubItems.Add(fi.DirectoryName);
+                    this.Refresh();
+                    Application.DoEvents();//klavye ve mouse tıklamalarına izin verir
+                    lblDurum.Text = fi.DirectoryName;
+                }
+            }
+            catch { }
+        
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            txtADRES.Text = e.Node.FullPath;
+            listView1.Items.Clear();
+            DosyaListele(txtADRES.Text, "*.*");
+        }
+
+        private void txtADRES_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode==Keys.Enter)
+            {
+                listView1.Items.Clear();
+                DosyaListele(txtADRES.Text, "*.*");
+            }
+        }
+
+        private void listeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            listView1.View = View.List;
+        }
+
+        private void detayToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            listView1.View = View.Details;
+        }
+
+        private void küçükResimToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            listView1.View = View.SmallIcon;
+        }
+
+        private void büyükResimToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            listView1.View = View.LargeIcon;
+        }
+
+        private void kopyalaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //kopyala
+            liste.Clear();
+            yapıştırToolStripMenuItem.Enabled = true;
+            for (int i = 0; i < listView1.SelectedItems.Count; i++)
+            {
+                //Dosyaadı,Boyutu,Klasör
+                string dosyaadi = listView1.SelectedItems[i].SubItems[0].Text;
+                string klasor = listView1.SelectedItems[i].SubItems[2].Text;
+                liste.Add(klasor + "/" + dosyaadi);
+            }
+        }
+
+        private void yapıştırToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //yapıştır
+            yapıştırToolStripMenuItem.Enabled = false;
+            Form f1 = new Form();
+            f1.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            f1.StartPosition = FormStartPosition.CenterScreen;
+            f1.Height = 50;
+            f1.Width= 150;
+            f1.BackColor = Color.Coral;
+            ProgressBar pb = new ProgressBar();
+            pb.Maximum = liste.Count;
+            pb.Minimum = 0;
+            pb.Height = 30;
+            pb.Width = 130;
+            pb.Left = 10;
+            pb.Top = 10;
+            f1.Controls.Add(pb);
+            f1.TopMost = true;
+            f1.Show();
+
+            for (int i = 0; i < liste.Count; i++)
+            {
+                pb.Value = i+1;
+                pb.Refresh();
+                string kaynak = liste[i].ToString();
+                string hedef = txtADRES.Text + "" + Path.GetFileName(kaynak);
+                File.Copy(kaynak, hedef, true);
+            }
+            listView1.Items.Clear();
+            DosyaListele(txtADRES.Text, "*.*");
+            f1.Close();
+        }
+
+        private void silToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            while (listView1.SelectedItems.Count>0)
+            {   string dosyaadi = listView1.SelectedItems[0].SubItems[0].Text;
+                string klasor = listView1.SelectedItems[0].SubItems[2].Text;
+                File.Delete(klasor + "/" + dosyaadi);
+                listView1.Items.RemoveAt(0);
+            }
+        }
+
+        private void listView1_DoubleClick(object sender, EventArgs e)
+        {
+            string dosyaadi = listView1.FocusedItem.SubItems[0].Text;
+            string klasor = listView1.FocusedItem.SubItems[2].Text;
+            System.Diagnostics.Process.Start(klasor + "/" + dosyaadi);
+        }
+        bool AramaDurumu = false;
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {//ara
+            AramaDurumu = !AramaDurumu;
+            if (AramaDurumu==true)
+            {
+                listView1.Items.Clear();
+                DosyaAra(txtADRES.Text, txtARA.Text);
+                lblDurum.Text = "Arama işlemi bitti";
+            }
+        }
+
+        void DosyaAra(string yol,string filtre)
+        {
+            try
+            {
+                if (AramaDurumu == false) return;
+                DosyaListele(yol, filtre);
+                string[] klasor = Directory.GetDirectories(yol);
+                for (int i = 0; i < klasor.Length; i++)
+                {
+                    DosyaAra(klasor[i], filtre);
+                    if (AramaDurumu == false) return;
+                }
+            }
+            catch { }
+        }
+    }
+}
